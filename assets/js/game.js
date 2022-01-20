25539064ecbe99dea937398a1bb248fd4359daba
Ethan Brockway <ebrockway01@gmail.com>
2022-01-19 19:11:34 -0600

    Object optimization

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,64 @@
 // var playerInfo.name = 'Clank McKrank';
 
 // You can also log multiple values at once like this
-console.log(playerInfo.name, playerInfo.attack, playerInfo.health);
 
 var randomNumber = function (min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
   return value;
 };
 
+var playerInfo = {
+  name: window.prompt("What is your robot's name?"),
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function () {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  },
+  refillHealth: function () {
+    if (this.money >= 7) {
+      window.alert("Refilling player's health by 20 for 7 coins.");
+      this.health += 20;
+      this.money -= 7;
+    } else {
+      window.alert("You do not have enough coins champ.");
+    }
+  },
+  upgradeAttack: function () {
+    if (player.money >= 7) {
+      window.alert("Upgrading your attack for 7 coins.");
+      this.attack += 6;
+      this.money -= 7;
+    } else {
+      window.alert("You dont have enough coins champ.");
+    }
+  },
+};
+var startGame = function () {
+  playerInfo.reset();
+  for (var i = 0; i < enemyInfo.length; i++) {
+    if (playerInfo.health > 0) {
+      window.alert("Welcome to Robot Gladiators! ROUND " + (i + 1) + " FIGHT");
+      var pickedEnemyObj = (pickedEnemyObj = enemyInfo[i]);
+      pickedEnemyObj.health = randomNumber(40, 60);
+      fight(pickedEnemyObj);
+      if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
+        var storeConfirm = window.confirm(
+          "The fight is over, visit the store before going back to battle?"
+        );
+        if (storeConfirm) {
+          shop();
+        }
+      }
+    } else {
+      window.alert("You have lost your robot in battle :/. Game Over...");
+      break;
+    }
+  }
+  endGame();
+};
 // fight function
 var fight = function (enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
@@ -90,17 +141,7 @@ var fight = function (enemy) {
     }
   }
 };
-var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
-  health: 100,
-  attack: 10,
-  money: 10,
-  reset: function () {
-    this.health = 100;
-    this.money = 10;
-    this.attack = 10;
-  },
-};
+
 var enemyInfo = [
   {
     name: "Megatron",
@@ -115,29 +156,7 @@ var enemyInfo = [
     attack: randomNumber(10, 14),
   },
 ];
-var startGame = function () {
-  playerInfo.reset();
-  for (var i = 0; i < enemyInfo.length; i++) {
-    if (playerInfo.health > 0) {
-      window.alert("Welcome to Robot Gladiators! ROUND " + (i + 1) + " FIGHT");
-      var pickedEnemyObj = (pickedEnemyObj = enemyInfo[i]);
-      pickedEnemyObj.health = randomNumber(40, 60);
-      fight(pickedEnemyObj);
-      if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
-        var storeConfirm = window.confirm(
-          "The fight is over, visit the store before going back to battle?"
-        );
-        if (storeConfirm) {
-          shop();
-        }
-      }
-    } else {
-      window.alert("You have lost your robot in battle :/. Game Over...");
-      break;
-    }
-  }
-  endGame();
-};
+
 var shop = function () {
   var shopOptionPrompt = window.prompt(
     "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
@@ -145,25 +164,12 @@ var shop = function () {
   switch (shopOptionPrompt) {
     case "refill":
     case "REFILL":
-      if (playerInfo.money >= 7) {
-        window.alert("Refilling player's health by 20 for 7 coins.");
-        playerInfo.health = playerInfo.health + 20;
-        playerInfo.money = playerInfo.money - 7;
-      } else {
-        window.alert("Not enough coins champ!");
-      }
+      playerInfo.refillHealth();
       break;
     case "upgrade":
     case "UPGRADE":
-      if (playerInfo.money > 7) {
-        window.alert("Upgrading player's attack by 6 for 7 coins.");
-        playerInfo.attack = playerInfo.attack + 6;
-        playerInfo.money = playerInfo.money - 7;
-      } else {
-        window.alert("Not enough coins champ!");
-      }
+      playerInfo.upgradeAttack();
       break;
-
     case "leave":
     case "LEAVE":
       window.alert("Leaving store...");
